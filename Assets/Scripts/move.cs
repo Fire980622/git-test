9a55223bd155dc2c9ce94689a62a48d335552c92
YangYunFeng <yangyunfeng@shiyue.com>
2023-06-14 15:47:03 +0800

    q'q'q

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -8,12 +8,7 @@ public class move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
         print("1111")
-=======
-      print("------")
-       print("------")
->>>>>>> 0525d783d5e8d3717c4759736e701df9dc94b7c2
     }
 
     // Update is called once per frame
