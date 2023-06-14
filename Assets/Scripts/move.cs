410f986af2ca66eaf2f76775331644b4a245f6c6
YangYunFeng <yangyunfeng@shiyue.com>
2023-06-14 11:41:16 +0800

    冲突测试

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -8,8 +8,8 @@ public class move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("这是分支1-1");
-        print("这是分支2");
+        print("这是分支7");
+        print("这是分支7");
     }
 
     // Update is called once per frame
